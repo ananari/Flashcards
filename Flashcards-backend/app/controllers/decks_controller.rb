cd11a4f1b3d3a86d2cb97184b13748435cbff36a
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-10-01 11:26:07 -0500

    delete and new done, still need to finish edit

--- Flashcards-backend/app/controllers/decks_controller.rb
+++ Flashcards-backend/app/controllers/decks_controller.rb
@@ -1,4 +1,6 @@
 class DecksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
     def index
         decks = Deck.all
         render json: decks, include: :cards
@@ -11,5 +13,17 @@ class DecksController < ApplicationController
 
     def set_deck
         deck = Deck.find(params[:id])
+    end
+
+    def create
+        deck = Deck.create(name: params[:name], user_id:  params[:user_id])
+        decks = Deck.all
+        render json: decks
+    end
+
+    def destroy
+        Deck.destroy(params[:id])
+        decks = Deck.all
+        render json: decks
     end
 end
--- Flashcards-frontend/index.js
+++ Flashcards-frontend/index.js
@@ -1,6 +1,8 @@
 //Global Constants
 const deckNavigation = document.getElementById('deck-navigation');
 const navigationBar = document.getElementById('navigation-bar');
+const myDecksDropdown = document.getElementById('my-decks-dropdown');
+const sharedDecksDropdown = document.getElementById('shared-decks-dropdown')
 const deckWindow = document.getElementById('deck-window');
 const cardWindow = document.querySelector('#card-window');
 const cardDiv = document.querySelector('#cards');
@@ -10,16 +12,19 @@ function fetchDecks() {
     fetch(decksURL)
         .then(response => response.json() )
         .then(deckData => {
-            displayMyDecksButton(deckData);
+            clearEverything
+            myDecksButton(deckData);
         })
 }
 
-function displayMyDecksButton(deckData) {
+function myDecksButton(deckData) {
     //create button for "My Decks"
     const myDecksButton = document.createElement('button');
     myDecksButton.innerText = 'My Decks';
+    
     //append to Deck Nevigation nav tag
     navigationBar.appendChild(myDecksButton);
+    
     //add event listener for click
     myDecksButton.addEventListener('click', (event) => {
         console.log("'My Decks' button has been clicked");
@@ -31,17 +36,32 @@ function displayMyDecksButton(deckData) {
 };
 
 function loadDecks(deckData) {
-    //display view name and instructions
+    //display decks banner
     const myDecksBanner = document.createElement('h1')
     myDecksBanner.innerText = `These are all the decks you've created!`;
     deckWindow.appendChild(myDecksBanner);
-    const instructions = document.createElement('h3');
-    instructions.innerText = `click on a deck to go into study mode`;
-    deckWindow.appendChild(instructions);
+
+    //create a "New Deck" button
+    const p = document.createElement('p');
+    p.innerText = 'Create a New Deck: '
+    const newDeckButton = document.createElement('button');
+    newDeckButton.innerText = `New Deck`;
+    p.appendChild(newDeckButton);
+    
+    //append to Deck window
+    deckWindow.appendChild(p);
+
+    //add event listener for click
+    newDeckButton.addEventListener('click', (event) => {
+        console.log("'New Deck' button has been clicked");
+        clearDeckWindow();
+        clearCardWindow();
+        clearCardDiv();
+        createDeckForm();
+    })
 
     //displays each deck with delete button on main window
     displayDecks(deckData)
-
 }
 
 function displayDecks(deckData) {
@@ -60,36 +80,129 @@ function displayDecks(deckData) {
             console.log('study button has been pushed');
             clearDeckWindow();
             displayDeck(deck.id);
-        })
+        });
 
         //edit deck button
-        const editDeckButton = document.createElement('button')
-        editDeckButton.innerText = ' Edit '
-        li.appendChild(editDeckButton)
+        const editDeckButton = document.createElement('button');
+        editDeckButton.innerText = ' Edit ';
+        li.appendChild(editDeckButton);
         editDeckButton.addEventListener('click', (event) => {
-            console.log('edit button has been pushed')
+            console.log('edit button has been pushed');
             
             //  PLACEDHOLDER FOR DISPLAYDECK FUNCTION
-        })
+        });
 
         //delete deck button
-        const deleteDeckButton = document.createElement('button')
-        deleteDeckButton.innerText = ' Delete '
-        li.appendChild(deleteDeckButton)
+        const deleteDeckButton = document.createElement('button');
+        deleteDeckButton.innerText = ' Delete ';
+        li.appendChild(deleteDeckButton);
         deleteDeckButton.addEventListener('click', (event) => {
-            console.log('delete button has been pushed')
-
-            //  PLACEDHOLDER FOR DELETEDECK FUNCTION
-        })
+            console.log('delete button has been pushed');
+            clearDeckWindow();
+            deleteDeck(deck.id);
+        });
 
         //append to main window area
         deckWindow.appendChild(li);
     }
 }
 
+function createDeckForm() {
+    //create Form for new deck
+    const newDeckForm = document.createElement('form');
+    
+    //label of name input box
+    const newDeckNameLabel = document.createElement('p');
+    newDeckNameLabel.innerText = "Name of new deck: ";
+    newDeckForm.appendChild(newDeckNameLabel);
+
+    //value of name input box
+    const newDeckNameInput = document.createElement('input');
+    newDeckNameInput.setAttribute('type', 'text');
+    newDeckForm.appendChild(newDeckNameInput);
+
+    //label of user input box
+    const newDeckUserLabel = document.createElement('p');
+    newDeckUserLabel.innerText = "User of new deck: ";
+    newDeckForm.appendChild(newDeckUserLabel);
+
+    //value of label input box
+    const newDeckUserInput = document.createElement('input');
+    newDeckUserInput.setAttribute('type', 'number');
+    newDeckForm.appendChild(newDeckUserInput);
+
+    //submit button
+    const newDeckSubmitButton = document.createElement('input');
+    newDeckSubmitButton.setAttribute('type', 'Submit')
+    newDeckForm.appendChild(newDeckSubmitButton);
+
+    deckWindow.appendChild(newDeckForm);
+
+    //add event listener and action for submit button
+    newDeckSubmitButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        newDeckNameValue = newDeckNameInput.value;
+            //console.log("New Deck Name Value = ", newDeckNameValue);
+        newDeckUserValue = newDeckUserInput.value;
+            //console.log("New User Name Value = ", newDeckUserValue);
+        clearDeckWindow();
+        clearCardWindow();
+        clearCardDiv();
+        createNewDeck(newDeckNameValue, newDeckUserValue);
+    })
+}
+
+function createNewDeck(newDeckNameValue, newDeckUserValue) {
+    let newDeckData = {
+        name: newDeckNameValue,
+        user_id: newDeckUserValue
+    }
+
+    let newDeckObject = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newDeckData)
+    }
+
+    fetch(decksURL, newDeckObject)
+        .then(response => response.json() )
+        .then( (deckData) => {
+            loadDecks(deckData);
+        })
+        .catch(error => console.log(error));
+}
+
+function deleteDeck(deckID) {
+    let deleteURL = `http://localhost:3000/decks/` + deckID;
+
+    let deleteObject = {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    fetch(deleteURL, deleteObject)
+    .then(response => response.json() )
+    .then( (deckData) => {
+        loadDecks(deckData);
+    })
+    .catch(error => console.log(error));
+}
+
 function clearDeckWindow() {
     while (deckWindow.hasChildNodes() ) {  
         deckWindow.removeChild(deckWindow.firstChild);
+    }
+}
+
+function clearNavigationBar() {
+    while (navigationBar.hasChildNodes() ) {  
+        navigationBar.removeChild(navigationBar.firstChild);
     }
 }
 
@@ -103,6 +216,13 @@ function clearCardDiv() {
     while (cardDiv.hasChildNodes() ) {  
         cardDiv.removeChild(cardDiv.firstChild);
     }
+}
+
+function clearEverything() {
+    clearCardWindow();
+    clearDeckWindow();
+    clearNavigationBar();
+    clearCardDiv();
 }
 
 
